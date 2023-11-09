26c181f96e09cb521669db411c8e15f996ba6c39
jdunn35 <143722158+jdunn35@users.noreply.github.com>
2023-11-09 10:59:55 -0500

    javascript1

---
+++ javascript1/script.js
@@ -0,0 +1,16 @@
+//window.alert("hello alert box!");//
+
+document.getElementById("number").innerHTML = 5+6;
+
+const x = 2;
+const y = 3;
+const z = x+y
+document.getElementsByClassName("sentence")[0].innerHTML = "The value of the variable z is " + z + ".";
+
+const FirstName = "Julianne";
+document.getElementById("name").innerHTML = FirstName;
+
+document.getElementById("feeling").innerHTML = "I'm happy";
+function changeFeeling() {
+        document.getElementById("feeling").innerHTML = "I'm tired";
+    }
